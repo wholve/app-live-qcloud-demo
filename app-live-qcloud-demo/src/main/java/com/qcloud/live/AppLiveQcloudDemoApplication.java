f00ba8b1543c740b620f11a86513a22009cf80f8
username <username@WHOLVE>
2017-05-01 17:07:45 +0800

    Change-Id: Ibe9e8d17d3af3ff7a07ef5d6ff8ffa0e54672ba6 Signed-off-by: username <username@WHOLVE>

---
+++ app-live-qcloud-demo/src/main/java/com/qcloud/live/AppLiveQcloudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qcloud.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLiveQcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLiveQcloudDemoApplication.class, args);
+	}
+}
