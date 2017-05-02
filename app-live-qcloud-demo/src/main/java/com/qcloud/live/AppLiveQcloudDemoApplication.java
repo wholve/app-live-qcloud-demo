5d3d3c195d0753282d1558d4b4e8f8ca38645cd7
username <username@WHOLVE>
2017-05-02 18:31:11 +0800

    update

--- app-live-qcloud-demo/src/main/java/com/qcloud/live/AppLiveQcloudDemoApplication.java
+++ app-live-qcloud-demo/src/main/java/com/qcloud/live/AppLiveQcloudDemoApplication.java
@@ -1,26 +1,22 @@
 package com.qcloud.live;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableAutoConfiguration
 public class AppLiveQcloudDemoApplication extends SpringBootServletInitializer{
 
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(new String[]{
-                "classpath*:config/spring-mvc.xml",
+                "classpath*:config/spring-mvc.xml"
         });
     }
     public static void main(String[] args) {
         SpringApplication.run(new String[]{
-                "classpath*:config/spring-mvc.xml",
+                "classpath*:config/spring-mvc.xml"
         }, args);
 
     }
